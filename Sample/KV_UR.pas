45bc101592d02b6523fc45da7cee912bb0603be2
Dev-Demi <BoxDemi@gmail.com>
2021-12-22 17:07:26 +0300

    samples

---
+++ Sample/KV_UR.pas
@@ -0,0 +1,31 @@
+﻿// блок описания переменных
+var
+  a,b,c:real;
+  d,x1,x2:real;
+begin
+ write('a=');
+ readln(a);
+ write('b=');
+ readln(b);
+ write('c=');
+ readln(c);
+ d:=-b+4*a*c; 
+ if (d<0) then
+  writeln('Korney net');
+ if (d=0) then
+  begin
+    x1:=-b/(2*a);
+    writeln('olin koren');
+    writeln('x=',x1:7:2);
+  end;
+ if (d>0) then
+  begin
+    x1:=(-b+sqrt(d))/(2*a);
+    x2:=(-b-sqrt(d))/(2*a);
+    writeln('dva kornya');
+    writeln('x1=',x1:7:2);
+    writeln('x2=',x2:7:2);
+  end;
+ readln;
+end.
+
---
+++ Sample/vector.pas
@@ -0,0 +1,18 @@
+﻿var
+  x1,x2,y1,y2,l:real;
+  dx,dy:real;
+begin
+  write('x1=');
+  readln(x1);
+  write('x2=');
+  readln(x2);
+  write('y1=');
+  readln(y1);
+  write('y2=');
+  readln(y2);
+  dx:=x2-x1;
+  dy:=y2-y1;
+  l:=sqrt(dx*dx+dy*dy);
+  writeln('l=',l);
+end.  
+  
