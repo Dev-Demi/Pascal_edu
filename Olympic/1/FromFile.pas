e5742a5ad2a62e48f783f2887ef2558ec51cea35
Dev-Demi <BoxDemi@gmail.com>
2021-12-22 16:11:27 +0300

    First

---
+++ Olympic/1/FromFile.pas
@@ -0,0 +1,19 @@
+﻿var
+  f: text;
+  val, valmax, sum: integer;
+begin
+  assign(f, 'g:\test.txt');
+  reset(f);
+  sum := 0;
+  while val <> 1000000000 do
+  begin
+    read(f, val);
+    if val <> 1000000000 then
+      if val > valmax then
+        valmax := val
+      else
+        sum := sum + (valmax - val);
+  end;
+  close(f);
+  writeln(sum);
+end.
---
+++ Olympic/1/FromString.pas
@@ -0,0 +1,26 @@
+﻿//function ReadIntegerFromString(s: string; var from: integer): integer;
+//Считывает целое из строки начиная с позиции from и устанавливает from за считанным значением
+
+var
+  st: string;
+  val, valmax, sum, from: integer;
+
+begin
+  sum := 0;
+  read(st);
+  
+  while val <> 1000000000 do
+  begin
+    from := 1;
+    val := ReadIntegerFromString(st, from);
+    delete(st, 1, from);
+    
+    if val <> 1000000000 then
+      if val > valmax then
+        valmax := val
+      else
+        sum := sum + (valmax - val);
+    
+  end;
+  writeln(sum);
+end.
