f3ffb8e198959b83e3534d98a880b158b02bc5bf
Dev-Demi <BoxDemi@gmail.com>
2022-01-10 14:01:26 +0300

    Create christmastree.pas

---
+++ Sample/christmastree.pas
@@ -0,0 +1,43 @@
+﻿// https://rextester.com/PSIJWX89218
+
+var
+  i:integer;
+  
+  procedure PrintChar(c:char;Count:integer);
+  var
+    i:integer; 
+  begin
+    for i:=1 to Count do
+      if random>0.95 then
+        begin
+         if c='*' then
+          write('o')
+         else
+          write(c);   
+        end
+        else
+          write(c); 
+  end;
+  
+  procedure PrintPartChristmasTree(LevelCount,MaxLevelCount:integer);
+  var
+    i:integer;
+    StarCount,SpaceCount:integer;
+  begin
+   StarCount:=1;
+   SpaceCount:=LevelCount-1;
+   for i:=1 to LevelCount do
+   begin
+    PrintChar(' ',SpaceCount+MaxLevelCount-LevelCount);
+    PrintChar('*',StarCount);
+    writeln;
+    StarCount:=StarCount+2;
+    SpaceCount:=SpaceCount-1;
+   end;
+  end;
+  
+begin
+  randomize;
+  for i:=3 to 8 do
+   PrintPartChristmasTree(i,8);  
+end.  
